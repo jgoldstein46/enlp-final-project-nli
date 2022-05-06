856b06854653eb180e84c2ba43f81c2933e30c8b
varunuppala <vu14@georgetown.edu>
2022-05-06 15:43:40 -0400

    Initial updates BERT

--- classifiers/ApproachTwo.py
+++ classifiers/ApproachTwo.py
@@ -1,5 +1,33 @@
 from classifiers.Classifier import NLI_Classifier_Base
+import tensorflow as tf
+from transformers import BertTokenizer, TFBertModel
+
+from keras.models import Sequential
+from keras.layers import Embedding, Bidirectional, LSTM, Dense, TimeDistributed, Dropout
+
 # TODO implement this class (or import it from huggingface if you like)
 class BERT_NLI_Classifier(NLI_Classifier_Base):
     def __init__(self, params):
-        pass
+        hidden_size = params["hidden_size"]
+        dropout = params["dropout"]
+        self.vocab = params["vocab"]
+        self.embedding_size = params["embedding_size"]
+        self.n_layers = params['n_layers']
+
+        classifier = Sequential()
+        embedding_layer = BERT_Wrapper()
+        
+
+
+class BERT_Wrapper(Model):
+
+  def __init__(self,hidden_size):
+    super(BERT_Wrapper, self).__init__()
+    self.encoder = TFBertModel.from_pretrained("bert-base-uncased", trainable=False)
+    self.dense = Dense(args.hidden_size)
+
+  def call(self, inputs, **kwargs):
+      outputs = self.encoder(inputs)
+      last_hidden_states = outputs[0] # The last hidden-state is the first element of the output tuple
+      output = self.dense(last_hidden_states)
+      return output
